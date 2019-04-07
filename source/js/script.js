9dcfa60df815aaa633b0178d6654a46a6fafb987
Moreorsea <dastast@mail.ru>
2019-04-07 12:12:57 +0300

    Доработка просмотра галереи

--- source/js/script.js
+++ source/js/script.js
@@ -1,8 +1,22 @@
 'use strict';
 
-var galleryImg = document.querySelector('.gallery__img');
+var galleryImg = document.querySelectorAll('.gallery__img'); //получила всеэлементы коллекции
+var getContainer = document.querySelector('.gallery__container');
+var container = document.querySelector('.conteiner');
+var imgSrc = ['img/IMG_1022.png', 'img/5.png', 'img/DSC08143.png', 'img/IMG_6924.png', 'img/8hVMhD8eWDs.png', 'img/8.png',
+ 'img/-6WvLcbWdl0.png', 'img/g__IwWjuKBw.png', 'img/IMG_1040.png', 'img/4.png', 'img/DSC00524.png', 'img/IMG_1039.png', 'img/3.png'];
+ var popupOpen = function (atr) {
+   container.classList.add('popup_bg');
+   };
 
-galleryImg.addEventListener = function () {
-  var img = alert(this);
+galleryImg.forEach((item) => { //перебираю каждый элемент
+  item.addEventListener('click', function (e) {
+  var img = e.currentTarget;
   console.log(img);
-}
+  img.getAttribute('src');
+  var newSrc = img.getAttribute('src');
+  console.log(newSrc);
+
+  popupOpen(img);
+  });
+});
