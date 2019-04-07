d895411fb3ff3a35f03e77ce0329807704086b5c
Moreorsea <dastast@mail.ru>
2019-04-07 12:50:18 +0300

    Добавление увеличенного изображения при клике

--- source/js/script.js
+++ source/js/script.js
@@ -1,13 +1,16 @@
 'use strict';
 
 var galleryImg = document.querySelectorAll('.gallery__img'); //получила всеэлементы коллекции
+console.log(galleryImg);
 var getContainer = document.querySelector('.gallery__container');
-var container = document.querySelector('.conteiner');
+var container = document.querySelector('.container');
 var imgSrc = ['img/IMG_1022.png', 'img/5.png', 'img/DSC08143.png', 'img/IMG_6924.png', 'img/8hVMhD8eWDs.png', 'img/8.png',
  'img/-6WvLcbWdl0.png', 'img/g__IwWjuKBw.png', 'img/IMG_1040.png', 'img/4.png', 'img/DSC00524.png', 'img/IMG_1039.png', 'img/3.png'];
- var popupOpen = function (atr) {
+var popupOpen = function (atr) {
    container.classList.add('popup_bg');
+   galleryImg.classList.add('popup_img');
    };
+
 
 galleryImg.forEach((item) => { //перебираю каждый элемент
   item.addEventListener('click', function (e) {
@@ -18,5 +21,6 @@ galleryImg.forEach((item) => { //перебираю каждый элемент
   console.log(newSrc);
 
   popupOpen(img);
+  imgBig(img);
   });
 });
