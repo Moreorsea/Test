a8a1399bd1ff31ffae2762c3d334403cf88d87bf
Moreorsea <dastast@mail.ru>
2019-04-07 13:20:59 +0300

    Работающая галерея

--- source/js/script.js
+++ source/js/script.js
@@ -1,14 +1,11 @@
 'use strict';
 
 var galleryImg = document.querySelectorAll('.gallery__img'); //получила всеэлементы коллекции
-console.log(galleryImg);
 var getContainer = document.querySelector('.gallery__container');
 var container = document.querySelector('.container');
-var imgSrc = ['img/IMG_1022.png', 'img/5.png', 'img/DSC08143.png', 'img/IMG_6924.png', 'img/8hVMhD8eWDs.png', 'img/8.png',
- 'img/-6WvLcbWdl0.png', 'img/g__IwWjuKBw.png', 'img/IMG_1040.png', 'img/4.png', 'img/DSC00524.png', 'img/IMG_1039.png', 'img/3.png'];
+var popupBg = document.querySelector('.popup_bg');
 var popupOpen = function (atr) {
    container.classList.add('popup_bg');
-   galleryImg.classList.add('popup_img');
    };
 
 
@@ -16,11 +13,16 @@ galleryImg.forEach((item) => { //перебираю каждый элемент
   item.addEventListener('click', function (e) {
   var img = e.currentTarget;
   console.log(img);
-  img.getAttribute('src');
-  var newSrc = img.getAttribute('src');
-  console.log(newSrc);
 
   popupOpen(img);
-  imgBig(img);
+
+  img.classList.add('popup_img');
+  var popupClose = function () {
+    container.addEventListener('click', function (){
+      container.classList.remove('popup_bg');
+      img.classList.remove('popup_img');
+    });
+  };
+  popupClose();
   });
 });
