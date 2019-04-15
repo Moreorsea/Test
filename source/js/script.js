63cf843fdf0a3ed8ec09cd1bda0a0f1f97272bc8
Moreorsea <dastast@mail.ru>
2019-04-15 12:20:56 +0300

    Добавление уменьшения изображения

--- source/js/script.js
+++ source/js/script.js
@@ -11,29 +11,70 @@ var popupOpen = function () {
 };
 
 
-galleryImg.forEach((item) => { //перебираю каждый элемент
-  item.addEventListener('click', function (e) {
-  var img = e.currentTarget;
-  var element = img.cloneNode(true);
-  console.log(img);
-  console.log(element);
 
+var opacityMin = 0;
+var opacityMax = 0.7;
+
+
+var fadeIn = function (time, element, opacityMax) { 
+  var element = document.querySelector('.popup');
+  var elementPopup = document.querySelector('.popup_bg');
+  var step = opacityMax/time;
+  console.log(step);
+
+
+  for (var i = 0; i <= opacityMax;) {
+    var opacity = opacityMin + (i + step);
+   i = i + step;
+   //element.style.opacity = opacity;
+   elementPopup.style.opacity = opacity;
+   console.log(element);
+   console.log(elementPopup);
+  }
+};
+
+var fadeOut = function (time, element, opacityMax) {
+  var element = document.querySelector('.popup');
+  var elementPopup = document.querySelector('.popup_bg');
+  var step = opacityMax/time;
+  console.log(step);
+
+  for (var i = 0; i < opacityMax; i = i + step) {
+    var opacity = opacityMax - (i + step);
+    console.log(opacity);
+    element.style.opacity = opacity;
+    elementPopup.style.opacity = opacity;
+  }
+}
+
+
+galleryImg.forEach((item) => { //перебираю каждый элемент
+  item.addEventListener('click', function (e) { //gallery__container
+  var img = e.currentTarget; //получение элемента
+  var element = img.cloneNode(true); //копирование элемента
   popupOpen();
 
-  popup.appendChild(element);
+  popup.appendChild(element); //добавление элемента в блок popup
 
-  element.classList.add('popup_img');
+  element.classList.add('popup_img'); //добавление элементу класса
+  element.classList.add('gallery__img--animation-visible');
 
-  console.log(element);
+
+  fadeIn(1000, document.querySelector('.popup_bg'), 0.7);  //opacity 0.7
+  //fadeIn(1000, document.querySelector('.popup_img'), 1);
 
   var popupClose = function () {
-    popup.addEventListener('click', function (){
+    container.addEventListener('click', function (){
       container.classList.remove('popup_bg');
       element.classList.remove('popup_img');
+      element.classList.add('gallery__img--animation-hidden');
       popup.removeChild(element);
+      fadeOut(1000, document.querySelector('.popup_bg'), 0.7);
     });
   };
   popupClose();
+  //fadeOut(2000, document.querySelector('.popup'), 1);
+  
   });
 });
 
@@ -43,14 +84,23 @@ var button = document.querySelector('.promo__button');
 var gallery = document.querySelector('.gallery');
 
 button.addEventListener('click', function(evt) {
-  window.scrollTo(0, 1400);
+  //window.scrollTo(0, 1400);
+  window.scrollTo({
+    top: 10000,
+    behavior: 'smooth'
+  })
 });
 
 
 
 //событие перемещения от галереи обратно вверх страницы
-var buttonBottom = document.querySelector('.button-bottom');
+var buttonBottom = document.querySelector('.button-bottom__img');
+console.log(buttonBottom);
 
-buttonBottom.addEventListener('click', function(evt) {
-  window.scrollTo(1500, 0);
+buttonBottom.addEventListener('click', function(e) {
+  //window.scrollTo(1500, 0);
+  window.scrollTo({
+    bottom: 1000,
+    behavior: 'smooth'
+  });
 });
