6c5b080a7e4024f800c19685c9f451d4af264e8f
Moreorsea <dastast@mail.ru>
2019-04-23 09:00:51 +0300

    Задание по скроллингу

---
+++ scroll/script.js
@@ -0,0 +1,31 @@
+'use strict';
+ 
+  var firstElem = document.querySelector('.one-container');
+  var twoElem = document.querySelector('.two-container');
+  var threeElem = document.querySelector('.three-container');
+  var fourElem = document.querySelector('.four-container');
+  var docEl = document.documentElement;
+
+  var getCoords = function (elem) {
+    //получаем координаты контейнера
+    var box = elem.getBoundingClientRect();
+    var body = document.body;
+
+    //считаем прокрутку страницы
+    var scrollTop = window.pageYOffset || docEl.scrollTop || body.scrollTop;
+    console.log(scrollTop);
+
+    //координаты нужного элемента = координаты элемента относительно окна + прокрутка
+    var scrollTop = box.top + scrollTop;
+    console.log(scrollTop);
+
+    window.onscroll =  function () {
+      window.scrollTo({
+        top: scrollTop,
+        left: 0,
+        behavior: 'smooth'
+      });
+    };
+  };
+  
+  getCoords(fourElem);
