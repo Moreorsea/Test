a2d474e7916dec0eaa28739223d06316f967281e
Moreorsea <dastast@mail.ru>
2019-04-24 21:24:11 +0300

    Скролл на проверку

--- scroll/script.js
+++ scroll/script.js
@@ -1,48 +1,58 @@
 'use strict';
  
-  var one = document.querySelector('#one');
-  var two = document.querySelector('#two');
-  var three = document.querySelector('#three');
-  var four = document.querySelector('#four');
-  var elem = document.querySelector('#four');
   var docEl = document.documentElement;
   var submit = document.querySelector(".submit");
   var inputText = document.querySelector("#text");
 
+//при клике на "скроллить" получаю номер блока, до которого нужно скроллить
+ submit.onclick = function () {
+   //получила номер блока, который ввел пользователь
+   var text = inputText.value;
+   console.log(text);
 
-
-  var getCoords = function (elem) {
-    //получаем координаты контейнера
-    //var box = elem.getBoundingClientRect();
-    var box = four.getBoundingClientRect();
+   //нахожу элемент по введенному пользователем числу
+   var index = document.querySelector('#a1[dataset="'+text+'"]');
+   console.log(index);
+  
+   //получить координату, до которой нужно скроллить
+   var getCoords = function (element) {
+    //получаю координату контейнера
+    var box = element.getBoundingClientRect();
+    console.log(box);
     var body = document.body;
-
+    
     //считаем прокрутку страницы
     var scrollTop = window.pageYOffset || docEl.scrollTop || body.scrollTop;
     console.log(scrollTop); 
 
     //координаты нужного элемента = координаты элемента относительно окна + прокрутка
-    var top = box.top + scrollTop;
-    console.log(top);     
-  };
+    var top = Math.round(box.top + scrollTop);
+    console.log(top);
+    
 
-    //при клике на "скроллить" получаю номер блока, до которого нужно скроллить
-    submit.onclick = function () {
-      
-      //получила номер блока
-      var text = inputText.value;
-      console.log(text);
+    //в цикле по шагам приближаюсь до точки, возле которой останавливается скроллинг
+   var sum = 50;
+
+   var timer = function () {
+     var timerId = setInterval(function(){
+       sum = sum + 10;
+       console.log(top);
      
+     if(sum > top) clearInterval(timerId);
+     console.log(sum);
+     window.scroll(0, sum);
+     },10);
+   };
 
-      //сюда подставлять элемент, идентификатор которого я вытащила
-      getCoords(four);
+   timer ();
+   return top;
+   };
 
-      //в цикле по шагам приближаюсь до точки, возле которой останавливается скроллинг
-       for (var i = 30; i < top; i = i + 250) {
-          var sum = 0;
-          sum = sum + i;
-          console.log(sum);  
-          window.scroll(0, sum);      
-        }
-  
-    };
+   
+
+   getCoords(index);
+
+   
+   
+ };
+
