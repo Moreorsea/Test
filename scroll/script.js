30d30ec025ef42331022557e68fcbcc285584102
Moreorsea <dastast@mail.ru>
2019-04-24 11:20:27 +0300

    Работа над скроллом

--- scroll/script.js
+++ scroll/script.js
@@ -1,47 +1,48 @@
 'use strict';
  
-  var firstElem = document.querySelector('.one-container');
-  var twoElem = document.querySelector('.two-container');
-  var threeElem = document.querySelector('.three-container');
-  var fourElem = document.querySelector('.four-container');
+  var one = document.querySelector('#one');
+  var two = document.querySelector('#two');
+  var three = document.querySelector('#three');
+  var four = document.querySelector('#four');
+  var elem = document.querySelector('#four');
   var docEl = document.documentElement;
-
-  /*var getValue = function () {
-    var inputValue = document.querySelector("#text").value;
-    console.log(inputValue);
-  };
-  document.querySelector("#submit").onclick = getValue();*/
-
-  var submit = document.querySelector("#submit");
+  var submit = document.querySelector(".submit");
   var inputText = document.querySelector("#text");
-  console.log(submit);
-  console.log(inputText);
-  submit.onclick = function () {
-    
-    var inputValue = inputText.getAttribute("value");
-    console.log(inputValue);
-  };
 
- /* var getCoords = function (elem) {
+
+
+  var getCoords = function (elem) {
     //получаем координаты контейнера
-    var box = elem.getBoundingClientRect();
+    //var box = elem.getBoundingClientRect();
+    var box = four.getBoundingClientRect();
     var body = document.body;
 
     //считаем прокрутку страницы
     var scrollTop = window.pageYOffset || docEl.scrollTop || body.scrollTop;
-    console.log(scrollTop);
+    console.log(scrollTop); 
 
     //координаты нужного элемента = координаты элемента относительно окна + прокрутка
-    var scrollTop = box.top + scrollTop;
-    console.log(scrollTop);
-
-    window.onscroll =  function () {
-      window.scrollTo({
-        top: scrollTop,
-        left: 0,
-        behavior: 'smooth'
-      });
-    };
+    var top = box.top + scrollTop;
+    console.log(top);     
   };
+
+    //при клике на "скроллить" получаю номер блока, до которого нужно скроллить
+    submit.onclick = function () {
+      
+      //получила номер блока
+      var text = inputText.value;
+      console.log(text);
+     
+
+      //сюда подставлять элемент, идентификатор которого я вытащила
+      getCoords(four);
+
+      //в цикле по шагам приближаюсь до точки, возле которой останавливается скроллинг
+       for (var i = 30; i < top; i = i + 250) {
+          var sum = 0;
+          sum = sum + i;
+          console.log(sum);  
+          window.scroll(0, sum);      
+        }
   
-  getCoords();*/
+    };
