2382f6cad2bb70c331c5d2d0abd6987abe408e00
Moreorsea <dastast@mail.ru>
2019-04-29 17:31:20 +0300

    Работа над циклом

--- scrollFix/script.js
+++ scrollFix/script.js
@@ -4,13 +4,26 @@ var element = document.querySelectorAll('.container');
 var one = document.querySelector('.one');
 var wrapper = document.querySelector('.wrapper');
 console.log(element);
+//var index = document.querySelector(`[dataset="${text}"]`);
+
 
 element.forEach((item) => {
-  document.onwheel = function (e) {
-    wrapper.style.transform = 'translateY(100vh)';
-  };
+  document.addEventListener('scroll', function (e) {
+    var img = e.currentTarget;
+    console.log(item);
+    for(var i = 0; i <4; ) {
+      item.style.transform = 'translateY(-100vh)' * i;
+      console.log(i);
+      i++;
+    }
+  });
 });
 
+var coord = document.documentElement.clientHeight; //(высота секции)
+console.log(coord);
+
 //есть 4 элемента section, ищу все через querySelectorAll, перебираю в forEach
-//вешаю обработчик onwheel на документ
-//при срабатывании события, у wrapper увеличить transform на высоту экрана
+//вешаю событие scroll на document
+//при срабатывании события, у section увеличить transform на высоту экрана
+//нужно зафиксировать в какую сторону была прокрутка, исходя из этого, transform будет + или -
+//
