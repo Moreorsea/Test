3d4608c91e3d93a0dc7461e43a672fa6ca392c45
Moreorsea <dastast@mail.ru>
2019-05-06 10:41:48 +0300

    Заработало

--- scrollFix/script.js
+++ scrollFix/script.js
@@ -2,6 +2,7 @@
 
 var element = document.querySelectorAll('.container');
 var wrapper = document.querySelector('.wrapper');
+var circle = document.querySelectorAll('.circle');
 console.log(element.length);
 //var currentSection = 0;
 //определить на какой секции произошел скролл
@@ -15,65 +16,35 @@ wrapper.addEventListener('wheel', function(e) {
   if (e.deltaY > 0) {
     if(currentElementIndex < element.length) {
       for (var i = 0; i < element.length; i++) {
-        element[i].style.transform = `translateY(-100vh)`;
-        console.log(element[i].style.transform = `translateY(-100vh)`);
+        var sum = -100 * currentElementIndex;
+        console.log(sum);
+        element[i].style.transform = `translateY(${sum}vh)`;
+        console.log(element[i].style.transform = `translateY(${sum}vh)`);
       }
-      currentElementIndex++;
+      circle[currentElementIndex].classList.add('circle--checked');
+      circle[currentElementIndex -1].classList.remove('circle--checked');
       console.log(currentElementIndex);
     } else {
-      currentElementIndex === element.length;
+      currentElementIndex === element.length - 1;
     }
   } else {
     if(currentElementIndex > 0) {
-      for (var i = 0; i < element.length; i++) {
-        element[i].style.transform = `translateY(0vh)`;
-        console.log(element[i].style.transform = `translateY(0vh)`);
-      }
       currentElementIndex--;
+      circle[currentElementIndex].classList.remove('circle--checked');
+      circle[currentElementIndex -1].classList.add('circle--checked');
+      for (var i = 3; i < element.length; i--) {
+        var sum = -100 * (currentElementIndex - 1);
+        console.log(sum);
+        console.log(currentElementIndex -1);
+        element[i].style.transform = `translateY(${sum}vh)`;
+        console.log(element[i].style.transform = `translateY(${sum}vh)`);
+      }
+      
+      console.log(circle[currentElementIndex].classList.remove('circle--checked'));
+      //circle[currentElementIndex -1].classList.add('circle--checked');
     } else {
       currentElementIndex === 0;
     }
   }
   console.log(currentElementIndex);
-
-//  var moveSection = function(index, number) {
-//number = -100vh;
-    //for (var i = 0; i < element.length; i++) {
-      //var sum = -100 * element[i].id;
-      //console.log(element[i]);
-      //console.log(sum);
-      //if(e.deltaY > 0) {
-        //setInterval
-    //element[i].style.transform = `translateY(number)`;
-    //  console.log(element[i].style.transform = `translateY(-100vh)`);
-    //} else {
-    //  element[i].style.transform = `translateY(0vh)`;
-  //  }
-    //}
-
-  //  console.log(element[index]);
-//  };
-  //moveSection(currentElementIndex, '-100vh');
 });
-
-//element.forEach(function(item, id) {
-  //document.addEventListener('scroll', function (e) {
-    //var img = e.currentTarget;
-
-    //for(var i = 0; i < 2; i++) {
-      //var sum = -100 * id;
-      //console.log(sum);
-      //item.style.transform = `translateY(${sum}vh)`;
-      //console.log(item.style.transform = `translateY(sumvh)`);
-    //}
-  //});
-//});
-
-var coord = document.documentElement.clientHeight; //(высота секции)
-console.log(coord);
-
-//есть 4 элемента section, ищу все через querySelectorAll, перебираю в forEach
-//вешаю событие scroll на document
-//при срабатывании события, у section увеличить transform на высоту экрана
-//нужно зафиксировать в какую сторону была прокрутка, исходя из этого, transform будет + или -
-//
