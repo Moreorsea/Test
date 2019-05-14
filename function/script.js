fb2750c96d5946bb04382285afae88c6708f6dd0
Moreorsea <dastast@mail.ru>
2019-05-14 13:27:23 +0300

    Дополнение

--- function/script.js
+++ function/script.js
@@ -11,7 +11,9 @@ var getContainer = function(selectorContainer, buttonLeft, buttonRight, speedAni
   var index = 0;
   var left = document.querySelector(buttonLeft);
   var right = document.querySelector(buttonRight);
-  var wrapperShift = wrapper.offsetWidth/2 - element[index].offsetWidth/2;
+  //var wrapperShift = wrapper.offsetWidth/2 - element[index].offsetWidth/2;
+  var wrapperShift = wrapper.offsetWidth/2 - (element[index].offsetWidth + element[index + 1].offsetWidth)/2;
+
   console.log(wrapperShift);
   wrapper.style.transform = `translateX(${wrapperShift}px)`;
   var currentElement = 0;
@@ -23,7 +25,11 @@ var getContainer = function(selectorContainer, buttonLeft, buttonRight, speedAni
       currentElement = element[index].offsetWidth;
       console.log(currentElement);
       var newElement = element[index+1].offsetWidth;
-      wrapperShift = wrapperShift - (currentElement/2 + newElement/2);
+      //wrapperShift = wrapperShift - (currentElement/2 + newElement/2);
+      console.log(element[index + 1].offsetWidth);
+      console.log(element[index + 2].offsetWidth);
+      console.log(wrapper.offsetWidth);
+      wrapperShift = wrapperShift - ((element[index + 1].offsetWidth + element[index + 2].offsetWidth)/2);
       console.log(wrapperShift);
       wrapper.style.transform = `translateX(${wrapperShift}px)`;
         if(index < 6) {
@@ -36,13 +42,17 @@ var getContainer = function(selectorContainer, buttonLeft, buttonRight, speedAni
           }
         }
       index++;
+    } else {
+      index == element.length;
     }
   });
 
   var indexBack = element.length -1;
   console.log(indexBack);
   left.addEventListener('click', function() {
-  wrapperShift = wrapperShift + element[indexBack].offsetWidth/2 + element[indexBack - 1].offsetWidth/2;
+    if (indexBack > 1) {
+  //wrapperShift = wrapperShift + element[indexBack].offsetWidth/2 + element[indexBack - 1].offsetWidth/2;
+    wrapperShift = wrapperShift + ((element[indexBack].offsetWidth + element[indexBack - 1].offsetWidth)/2);
     console.log(wrapperShift);
     wrapper.style.transform = `translateX(${wrapperShift}px)`;
     element[indexBack].style.opacity = '0';
@@ -53,6 +63,9 @@ var getContainer = function(selectorContainer, buttonLeft, buttonRight, speedAni
       element[indexBack + 1].style.opacity = '1';
     }
     indexBack--;
+  } else {
+    indexBack == 1;
+  }
 });
 
   container.style.transition = `ease ${speedAnimation}ms`;
