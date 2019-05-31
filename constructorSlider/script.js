d02e5e1c7550cfebf9263d7dc3b0318a6492fd8e
Moreorsea <dastast@mail.ru>
2019-05-31 17:13:07 +0300

    Не работает опять

--- constructorSlider/script.js
+++ constructorSlider/script.js
@@ -15,42 +15,42 @@ class Slide {
     for(var i = 0; i < this.elements.length; i++) {
       this.elements[i].style.transform = `translateX(668px)`;
     }
-    var index = 0;
+    this.index = 0;
   }
   slide(side) {
     switch(side) {
       case 'left': {
-        if(index > 0) {
-          element[index +1].style.visible = 'hidden';
-          var secondSum = (elements[index].offsetWidth + elements[index-1].offsetWidth)/2;
+        if(this.index > 0) {
+          this.elements[this.index +1].style.visible = 'hidden';
+          var secondSum = (this.elements[this.index].offsetWidth + this.elements[this.index-1].offsetWidth)/2;
           var wrapperWidth = this.container.offsetWidth/2;
           wrapperWidth = wrapperWidth - secondSum;
-          for(var i = elements.length - 1; i >= 0; i--) {
+          for(var i = this.elements.length - 1; i >= 0; i--) {
             this.elements[i].style.transform = `translateX(${wrapperWidth}px)`;
           }
-          elements[index+1].style.visibility = 'hidden';
-          elements[index-1].style.visibility = 'visible';
+          this.elements[this.index+1].style.visibility = 'hidden';
+          this.elements[this.index-1].style.visibility = 'visible';
         } else {
-            index === 0;
+            this.index === 0;
           }
-        index--;
+        this.index--;
       };
       break;
       case 'right': {
-        if(index < 6) {
-          var secondSum = (elements[index+1].offsetWidth + elements[index+2].offsetWidth)/2;
+        if(this.index < 6) {
+          var secondSum = (this.elements[this.index+1].offsetWidth + this.elements[this.index+2].offsetWidth)/2;
           wrapperWidth = wrapperWidth - secondSum;
-          for(var i = 0; i < elements.length; i++) {
-            elements[i].style.transform = `translateX(${wrapperWidth}px)`;
+          for(var i = 0; i < this.elements.length; i++) {
+            this.elements[i].style.transform = `translateX(${wrapperWidth}px)`;
           }
-          elements[index].style.visibility = 'hidden';
-          elements[index+2].style.visibility = 'visible';
+          this.elements[this.index].style.visibility = 'hidden';
+          this.elements[this.index+2].style.visibility = 'visible';
         } else {
-          index === 6;
-          elements[index].style.visibility = 'visible';
-          elements[index+1].style.visibility = 'visible';
+          this.index === 6;
+          this.elements[this.index].style.visibility = 'visible';
+          this.elements[this.index+1].style.visibility = 'visible';
         }
-        index++;
+        this.index++;
       };
       break;
       default: return;
@@ -61,4 +61,4 @@ class Slide {
 }
 }
 
-var newSlide = new Slide('left', 'right', 'wrapper');
+var newSlide = new Slide(left, right, wrapper);
