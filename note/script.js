cb73359354b038effa78f6994536f72cf6ee3c60
Moreorsea <dastast@mail.ru>
2019-06-10 10:30:19 +0300

    Проблема с объектом

--- note/script.js
+++ note/script.js
@@ -12,28 +12,25 @@ class Note {
             {
                 noteName: 'Заметка 1',
                 text: 'Ели мясо мужики',
-                work: 'В работе',
-                done: 'Сделано'
+                checked: 'В работе'
               },
               {
                 noteName: 'Заметка 2',
                 text: 'Пивом запивали',
-                work: 'В работе',
-                done: 'Сделано'
+                checked: 'В работе'
               },
               {
                 noteName: 'Заметка 3',
                 text: 'О чем конюх говорил',
-                work: 'В работе',
-                done: 'Сделано'
+                checked: 'В работе'
               },
               {
                 noteName: 'Заметка 4',
                 text: 'Они не понимали',
-                work: 'В работе',
-                done: 'Сделано'
+                checked: 'В работе'
               }
           ];
+          console.log(this.noteElements[1]);
           this.number = number;
           this.createNote(number);
     }
@@ -42,7 +39,6 @@ class Note {
        var listItem = document.createElement('li');
        listItem.classList.add('note-list__item');
        listItem.innerHTML = '<label class="note-list__item-wrapper"><div class="note-list__item-wrapper-text"><p class="note-list__item-name"></p><p class="note-list__item-work"></p><p class="note-list__item-done"></p></div><input type="checkbox" class="note-list__item-input"><span class="note-list__item-text"></span></label>';
-       //var div = listItem.firstChild;
  
        //скопировать шаблон, чтобы получить пустую разметку заметки
        var element = listItem.cloneNode(true);
@@ -53,20 +49,16 @@ class Note {
        element.querySelector('.note-list__item-name').innerHTML = this.noteElements[this.number].noteName;
        element.querySelector('.note-list__item-text').innerHTML = this.noteElements[this.number].text;
        var itemWork =  element.querySelector('.note-list__item-work');
-       itemWork.innerHTML = this.noteElements[this.number].work;
-       var itemDone = element.querySelector('.note-list__item-done');
-       itemDone.innerHTML = this.noteElements[this.number].done;
+       itemWork.innerHTML = this.noteElements[this.number].checked;
        var checkbox = element.querySelector('.note-list__item-input');
 
        //реакция на выбор заметки
        checkbox.addEventListener('change', function () {
         element.classList.toggle('note-list__item--checked');
         if(element.classList.toggle('note-list_item--checked') === true) {
-            itemWork.style.display = 'none';
-            itemDone.style.display = 'block';
+            itemWork.innerHTML = 'Сделано';
         } else {
-            itemWork.style.display = 'block';
-            itemDone.style.display = 'none';
+            itemWork.innerHTML = 'В работе';
         }
     });
     this.noteList.appendChild(element);
@@ -78,21 +70,23 @@ class List {
     //отсортировать список
     constructor(container){
      this.noteList = container;
-     //this.newNote0 = new Note(noteList, 0);
-     //this.newNote1 = new Note(noteList, 1);
-     //this.newNote2 = new Note(noteList, 2);
-     //this.newNote3 = new Note(noteList, 3);
      this.arrNote = [new Note(noteList, 0), new Note(noteList, 1), new Note(noteList, 2), new Note(noteList, 3)];
      console.log(this.arrNote);
      this.createList();
     }
     createList (){
-      console.log(this.noteList.children);
+      //console.log(this.noteList.children);
       var elements = document.querySelectorAll('.note-list__item');
       console.log(elements);
       
       console.log(this.arrNote[2]);
-      var checkbox = noteList.querySelector('.note-list__item-input');
+      //var checkbox = elements.querySelector('.note-list__item-input');
+      //console.log(checkbox);
+      noteList.addEventListener('click', function(evt){
+         console.log('Выводится');
+         var element = evt.target;
+         
+      });
     }
 }
 
