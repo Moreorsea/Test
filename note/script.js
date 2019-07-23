99c7da040756f0daac8004ac54d789aba9533dbb
Moreorsea <dastast@mail.ru>
2019-07-23 18:33:44 +0300

    .

--- note/script.js
+++ note/script.js
@@ -9,12 +9,11 @@ class Note {
     constructor (info) {
         this.elementInfo = info;
         this.checked = info.checked;
-        //this.createNote(this.transferNote);
-
+        this.id = info.id;
     }
+  
     createNote(transferNote){
     //создать HTML-разметку
-    console.log(this.elementInfo);
     var listItem = document.createElement('li');
     listItem.classList.add('note-list__item');
     listItem.setAttribute('id', '');
@@ -29,7 +28,6 @@ class Note {
     elementItem.querySelector('.note-list__item-text').innerHTML = this.elementInfo.text;
     elementItem.querySelector('.note-list__item-work').innerHTML = 'В работе';
 
-
     elementItem.querySelector('.note-list__item-input').addEventListener('click', () => {
       this.checked = !this.checked;
       transferNote(this);
@@ -39,6 +37,8 @@ class Note {
       if(this.checked) {
         elementItem.querySelector('.note-list__item-work').innerHTML = 'Сделано';
         elementItem.classList.add('note-list__item--checked');
+        
+        this.elementInfo.checked = this.checked;
         var currentElement = elementItem;
         removeElement(elementItem);
         noteList.appendChild(currentElement);
@@ -46,6 +46,7 @@ class Note {
         elementItem.querySelector('.note-list__item-work').innerHTML = 'В работе';
         elementItem.classList.remove('note-list__item--checked');
         var currentElement = elementItem;
+        this.elementInfo.checked = this.checked;
         removeElement(elementItem);
         noteList.insertBefore(currentElement, noteList.firstChild);
       }
@@ -67,22 +68,26 @@ class List {
       {
           noteName: 'Заметка 1',
           text: 'Ели мясо мужики',
-          checked: false
+          checked: false,
+          id: 0
         },
         {
           noteName: 'Заметка 2',
           text: 'Пивом запивали',
-          checked: false
+          checked: false,
+          id: 1
         },
         {
           noteName: 'Заметка 3',
           text: 'О чем конюх говорил',
-          checked: false
+          checked: false,
+          id: 2
         },
         {
           noteName: 'Заметка 4',
           text: 'Они не понимали',
-          checked: false
+          checked: false,
+          id: 3
         }
     ];
      this.renderList();
@@ -90,35 +95,52 @@ class List {
 
     transferNote (element) {
       //здесь будут перекидываться заметки
+     element.elementInfo.checked = true;
       var elementId = element.elementInfo.id;
-      function getList (array) {
-        for(var i = 0; i < array.length; i++) {
-          if(elementId === array[i].elementInfo.id) {
-            array.splice(i, 1);
-          }
-        }
-      };
-      
+      this.listUnchecked = [];
+      this.listChecked = [];
+      var index = 0;
       if(element.checked) {
-        this.listChecked.push(element);
-        getList(this.listUnchecked);
-        if(this.listChecked.length === 4) {
-          this.listUnchecked = [];
-        }
-        this.sortArray();
-      }  else {
-          this.listUnchecked.push(element);
-          getList(this.listChecked);
-          if(this.listUnchecked.length === 4) {
-            this.listChecked = [];
+        this.noteElements.forEach(() => {
+          if(this.noteElements[index].checked === false) {
+            this.listUnchecked.push(this.noteElements[index]);
+          } else {
+            this.listChecked.push(this.noteElements[index]);
           }
+          index++;
+          
+        });
+        this.sortArray();
+        console.log(this.listChecked);
+          console.log(this.listUnchecked);
+      } else {
+         element.elementInfo.checked = false;
+          this.noteElements.forEach(() => {
+            if(this.noteElements[index].checked === false) {
+              this.listUnchecked.push(this.noteElements[index]);
+              if(this.listUnchecked.length === 4) {
+                this.listChecked = [];
+              }
+            } else {
+              this.noteElements[index].id === elementId;
+              if(this.noteElements[index].id === elementId) {
+                this.listChecked.splice(this.noteElements[index], 1);
+              }
+            }
+            index++;
+            console.log(this.listChecked);
+            console.log(this.listUnchecked);
+          });
           this.sortArray();
-        }
+          console.log(this.listChecked);
+          console.log(this.listUnchecked);
+      }
     }
 
     sortArray () {
       this.listChecked.sort((prev, next) => prev.elementInfo.id - next.elementInfo.id);
       console.log(this.listChecked);
+      
       this.listUnchecked.sort((prev, next) => prev.elementInfo.id - next.elementInfo.id);
       console.log(this.listUnchecked);
       
@@ -127,12 +149,12 @@ class List {
     }
   
     renderList (createNote){
-      console.log(this.noteList);
+      //console.log(this.noteList);
      this.noteList.innerHTML = '';
      //this.createNote(this.transferNote());
-     console.log(this.noteElements);
+     //console.log(this.noteElements);
       
-      console.log(this.newArray);
+     // console.log(this.newArray);
       this.noteElements.forEach(() => {
         new Note(this.noteElements[this.index]).createNote((element)=>{
           this.transferNote(element);
